d2e1706588bd5ec3bd2305f311fd639c39146cc5
Vitor-Manoel565 <vitoralvesbvgs@gmail.com>
2022-10-18 14:14:23 -0300

    CRUD completed

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-import Express, { response } from "express";
+import Express from "express";
 
 const app = Express();
-app.use(Express.json())
+app.use(Express.json());
 
 const games = [
   {
-    "id": 1,
-    "name": "Valorant",
+    id: 1,
+    name: "Valorant",
   },
 ];
 
@@ -14,38 +14,38 @@ app.get("/", (request, response) => {
   response.status(200).send("Meu curso!");
 });
 
-
-app.get("/games",(request,response)=>{ //metodo de get
-    response.status(200).json(games)
+app.get("/games", (request, response) => {
+  //metodo de get
+  response.status(200).json(games);
 });
 
-app.get('/games/:id',(request,response)=>{ //metodo que busca por id
-    let index = buscaGame(request.params.id);
-    response.json(games[index])
-})
-
-
-app.post('/games',(request,response)=>{
-    games.push(request.body); //adiciona um post
-    response.status(201).send('Livro cadastrado!')
+app.get("/games/:id", (request, response) => {
+  //metodo que busca por id
+  let index = buscaGame(request.params.id);
+  response.json(games[index]);
 });
 
-app.put('/games/:id',(request,response)=>{ //metodo de atualização
-    let index = buscaGame(request.params.id);
-    games[index].name = request.body.name;
-    response.json(games)
-})
+app.post("/games", (request, response) => {
+  games.push(request.body); //adiciona um post, push adiciona um item no array
+  response.status(201).send("Livro cadastrado!");
+});
 
+app.put("/games/:id", (request, response) => {
+  //metodo de atualização
+  let index = buscaGame(request.params.id);
+  games[index].name = request.body.name;
+  response.json(games);
+});
 
-app.delete('/games/:id',(request,response)=>{ //metodo de deletar
-    let index = buscaGame(request.params.id);
-    games.splice(index,1);
-    response.send(`O livro ${request.params.id} foi deletado com sucesso!`);
-})
-    
+app.delete("/games/:id", (request, response) => {
+  //metodo de deletar
+  let index = buscaGame(request.params.id);
+  games.splice(index, 1); //exclui item na posição do index
+  response.send(`O livro ${request.params.id} foi deletado com sucesso!`);
+});
 
-function buscaGame(id){
-    return games.findIndex(game => game.id == id); // Busca id que seja igual ao que foi passado no corpo da requisição
+function buscaGame(id) {
+  return games.findIndex((game) => game.id == id); // Busca id que seja igual ao que foi passado no corpo da requisição
 }
 
 export default app;
